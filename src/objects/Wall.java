dc5975a5ece9d48b6e9a9ed001039e4772829b62
Gayatri Pise <gpise@mail.sfsu.edu>
2018-04-23 23:47:43 -0700

    add wall and tank collision

--- src/objects/Wall.java
+++ src/objects/Wall.java
@@ -6,36 +6,32 @@ import java.io.File;
 
 public class Wall extends GameObject {
 
-    private Boolean isDestructible;
+  private Boolean isDestructible;
 
+  public Wall(int x, int y, Boolean isDestructible) {
 
-
-  public Wall(int x, int y, Boolean isDestructible ){
-
-    try{
-      if(isDestructible){
+    try {
+      if (isDestructible) {
         image = ImageIO.read(new File("resources/wall.png"));
-      }else{
+      } else {
         image = ImageIO.read(new File("resources/wall_indestructible.png"));
       }
       this.x = x;
       this.y = y;
       this.isDestructible = isDestructible;
-    }catch (Exception e){
+    } catch (Exception e) {
       System.out.println("Exception while creating new wall object.");
     }
   }
 
-
-
-  public Wall(Image image, int x, int y, Boolean isDestructible ){
+  public Wall(Image image, int x, int y, Boolean isDestructible) {
     this.x = x;
     this.y = y;
     this.image = image;
     this.isDestructible = isDestructible;
   }
 
-
-
-
+  public boolean isDestructible() {
+    return this.isDestructible;
+  }
 }
